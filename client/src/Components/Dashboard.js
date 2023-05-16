41018046bd04c1c9eb79cc84037f4487f08dbd20
Inamkhanpk <inam_khan1233@yahoo.com>
2023-05-16 22:55:08 +0500

    add readme

--- client/src/Components/Dashboard.js
+++ client/src/Components/Dashboard.js
@@ -4,55 +4,82 @@ import { useNavigate } from "react-router-dom";
 import { getFingerprint } from "react-fingerprint";
 import axios from "axios";
 import {
-  takefingerprintEndpoint,
-  getfingerprintEndpoint,
+  takefingerprintEndpoint
 } from "../services/axiosInceptor";
 import { toast } from "react-toastify";
 import moment from "moment";
 
 const Dashboard = () => {
-  const { setUserInfo, users, isAuthenticated, setAuthentic } =
-    useContext(AuthContext);
+  const { setUserInfo, users, setAuthentic } = useContext(AuthContext);
   const [data, setData] = useState([]);
   //   const usr = JSON.parse(localStorage.getItem("user"));
   const [updateList, setUpdateList] = useState(false);
   //   console.log(users, isAuthenticated, users);
   const navigate = useNavigate();
+  console.log(users)
 
-  useEffect(() => {
-    if (users !== null) {
+  useEffect(() => { 
+    console.log("useEffect")
+    if(updateList === true ){
       axios
         .get(`http://localhost:4000/getFingerPrintHistory/${users?.id}`)
         .then((res) => {
-          console.log(res)
+          console.log(res);
           //   console.log(res.data.findUserFingerprintHistory[0].fingerprintData);
-
+        //   const dataf = localStorage.setItem("data",JSON.stringify(res.data.findUserFingerprintHistory[0].fingerprintData))
+        //    const datafp = JSON.parse(dataf)
+        //    console.log(datafp)
           setData(res.data.findUserFingerprintHistory[0].fingerprintData);
           setUpdateList(false);
+          console.log(updateList)
+        //   localStorage.setItem("data",res.data.findUserFingerprintHistory[0].fingerprintData)
         })
         .catch((err) => {
           console.log(err);
         });
     }
-  }, [updateList, users]);
+    
+  }, [updateList]);
+
+
+//   useEffect(() => { 
+    
+//       axios
+//         .get(`http://localhost:4000/getFingerPrintHistory/${users?.id}`)
+//         .then((res) => {
+//           console.log(res);
+//           //   console.log(res.data.findUserFingerprintHistory[0].fingerprintData);
+
+//           const dataf = localStorage.setItem("data",JSON.stringify(res.data.findUserFingerprintHistory[0].fingerprintData))
+//            const datafp = JSON.PARSE(dataf)
+//           setData(datafp);
+//           setUpdateList(false);
+//           localStorage.setItem("data",res.data.findUserFingerprintHistory[0].fingerprintData)
+//         })
+//         .catch((err) => {
+//           console.log(err);
+//         });
+    
+//   }, []);
 
   const handlePrint = async () => {
     let fingerprint = await getFingerprint(); // fingerprint id
-
+   
     axios
       .post(takefingerprintEndpoint, { fingerprint, userId: users.id })
       .then((res) => {
         if (res.data.message === "Fingerprint data saved successfully") {
           toast.success("Fingerprint data saved successfully");
+          setUpdateList(true);
         }
       });
-
-    setUpdateList(true);
+    
   };
 
   const handleLogout = () => {
     try {
       localStorage.removeItem("token");
+      localStorage.removeItem("data");
       setAuthentic(false);
       setUserInfo(null);
       navigate("/signin");
@@ -96,14 +123,14 @@ const Dashboard = () => {
               </tr>
             </thead>
             <tbody className="bg-white divide-y">
-              {data.map((item) => (
+              {  data ? data?.map((item) => (
                 <tr key={item.id} className="text-sm text-gray-500">
                   <td className="px-4 py-3">
                     {moment(item.time).format("hh:mm:ss a")}
                   </td>
                   <td className="px-4 py-3">{item.fingerprint}</td>
                 </tr>
-              ))}
+              ))     : "No FingerPrint Detected" }
             </tbody>
           </table>
         </div>
